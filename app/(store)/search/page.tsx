22f952ae4a6ba1daadee3190440d31787373608b
Nenad <nenadmobmail@gmail.com>
2024-12-03 23:47:13 +0100

    setup and header

---
+++ app/(store)/search/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchPage = async ({
+  searchParams,
+}: {
+  searchParams: {
+    query: string;
+  };
+}) => {
+  const { query } = await searchParams;
+
+  return <div>SearchPage for {query}</div>;
+};
+
+export default SearchPage;
